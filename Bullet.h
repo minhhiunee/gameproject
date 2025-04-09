b1e0e53522fc1e6d810fd01ff18d80ba6d60b87e
Nguyễn Minh Hiếu <88912588+minhhiunee@users.noreply.github.com>
2025-04-09 15:18:29 +0700

    Update Bullet.h

--- Bullet.h
+++ Bullet.h
@@ -1,19 +1,26 @@
 #ifndef BULLET_H
 #define BULLET_H
+#include <SDL.h>
 
 class Bullet {
 private:
     int x, y;
     bool active;
+    SDL_Texture* texture;
 
 public:
-    Bullet();
+    Bullet(SDL_Renderer* renderer);
+    Bullet(const Bullet& other); 
+    Bullet& operator=(const Bullet& other); 
+    ~Bullet();
     void update();
     void shoot(int startX, int startY);
     bool isActive() const { return active; }
     int getX() const { return x; }
     int getY() const { return y; }
     void deactivate() { active = false; }
+    SDL_Texture* getTexture() const { return texture; }
+    SDL_Rect getRect() const;
 };
 
 #endif
