19ad4b3cd3b3085f803f3fd6b8822ca699da6f28
Nguyễn Minh Hiếu <88912588+minhhiunee@users.noreply.github.com>
2025-04-09 15:15:58 +0700

    Update Object.cpp

--- Object.cpp
+++ Object.cpp
@@ -1,9 +1,34 @@
 #include "Object.h"
+#include <SDL.h>
+#include <SDL_image.h>
+#include <cstdlib>
+using namespace std;
 
-Object::Object(int windowWidth, int windowHeight)
-    : x(windowWidth),
-      y(windowHeight - OBJECT_SIZE - 50),
-      active(true) {}
+Object::Object(int windowWidth, int windowHeight, SDL_Renderer* renderer)
+    : x(windowWidth), y(windowHeight - 330), active(true) {
+    // Tải 5 texture vật cản
+    textures.push_back(IMG_LoadTexture(renderer, "tree 1.png"));
+    textures.push_back(IMG_LoadTexture(renderer, "tree 2.png"));
+    textures.push_back(IMG_LoadTexture(renderer, "stone 1.png"));
+    textures.push_back(IMG_LoadTexture(renderer, "stone 2.png"));
+    textures.push_back(IMG_LoadTexture(renderer, "banana skin.png"));
+
+    // Kiểm tra lỗi khi tải texture
+    for (size_t i = 0; i < textures.size(); ++i) {
+        if (!textures[i]) {
+            SDL_Log("Không thể tải texture object%zu.png: %s", i + 1, IMG_GetError());
+        }
+    }
+
+    // Chọn ngẫu nhiên texture ban đầu
+    currentTexture = textures[rand() % 5];
+}
+
+Object::~Object() {
+    for (SDL_Texture* texture : textures) {
+        if (texture) SDL_DestroyTexture(texture);
+    }
+}
 
 void Object::update() {
     x -= 5;
@@ -11,4 +36,5 @@ void Object::update() {
 
 void Object::reset(int windowWidth) {
     x = windowWidth;
+    currentTexture = textures[rand() % 5];
 }
