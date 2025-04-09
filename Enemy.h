118d610845fbd8b021dc0cf201a2c19873ac0bf8
Nguyễn Minh Hiếu <88912588+minhhiunee@users.noreply.github.com>
2025-04-09 15:15:42 +0700

    Update Enemy.h

--- Enemy.h
+++ Enemy.h
@@ -2,13 +2,17 @@
 #define ENEMY_H
 
 #include <SDL.h>
+#include <vector> // Thêm vector để lưu danh sách texture
 
 class Enemy {
 public:
     int x, y;
     bool active;
+    std::vector<SDL_Texture*> textures; // Danh sách các texture
+    SDL_Texture* currentTexture;        // Texture hiện tại được chọn
 
-    Enemy(int startX, int startY);
+    Enemy(int startX, int startY, SDL_Renderer* renderer);
+    ~Enemy();
     void update();
     void reset();
     SDL_Rect getRect();
