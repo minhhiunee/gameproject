c84e0b751c544f49ce6927cd41a97c7b66c3d1ee
Nguyễn Minh Hiếu <88912588+minhhiunee@users.noreply.github.com>
2025-04-09 15:16:55 +0700

    Update Player.h

--- Player.h
+++ Player.h
@@ -2,19 +2,35 @@
 #define PLAYER_H
 
 #include <SDL.h>
+#include <vector>
+#include "main.h"
+using namespace std;
 
 class Player {
 public:
     int x, y;
+    int initialY;
+    int width;           // Kích thước chung (dùng cho RUNNING)
+    int height;
+    int flyingWidth;     // Kích thước riêng cho FLYING
+    int flyingHeight;
     int speed;
     int score;
     bool isJumping;
-    int jumpHeight;
+    double jumpHeight;
+    vector<SDL_Texture*> runTextures;
+    SDL_Texture* textureFlying;
+    int currentFrame;
+    int frameDelay;
+    int frameCounter;
 
-    Player(int startX, int startY);
+    Player(int startX, int startY, SDL_Renderer* renderer);
+    ~Player();
     void updateJump();
     void movePlane(const Uint8* keystates);
-    SDL_Rect getRect();
+    void updateAnimation();
+    SDL_Rect getRect(GameState state); 
+    SDL_Texture* getCurrentTexture(GameState state);
 };
 
 #endif
