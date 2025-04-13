65ed1c4ed80e4393644e112dd27acfa56e8a88cc
Nguyễn Minh Hiếu <88912588+minhhiunee@users.noreply.github.com>
2025-04-13 10:16:58 +0700

    Khai báo thêm các hàm để bắn và tạo texture nổ

--- Enemy.h
+++ Enemy.h
@@ -2,20 +2,37 @@
 #define ENEMY_H
 
 #include <SDL.h>
-#include <vector> // Thêm vector để lưu danh sách texture
-
+#include <vector>
+#include "Bullet.h"
+using namespace std;
 class Enemy {
 public:
     int x, y;
     bool active;
-    std::vector<SDL_Texture*> textures; // Danh sách các texture
-    SDL_Texture* currentTexture;        // Texture hiện tại được chọn
+    vector<SDL_Texture*> textures;
+    SDL_Texture* currentTexture;
+    Uint32 lastShotTime; // Thời gian bắn đạn cuối cùng
+    static const Uint32 shootCooldown = 1500; // Cooldown 1.5 giây
+
+    // Thuộc tính cho vụ nổ
+    SDL_Texture* explosionTexture;
+    vector<SDL_Texture*> explosionTextures; // Lưu trữ các texture vụ nổ
+    SDL_Texture* currentExplosionTexture; // Texture vụ nổ hiện tại
+    bool showExplosion;
+    int explosionX, explosionY;
+    Uint32 explosionStartTime;
+    static const Uint32 EXPLOSION_DURATION = 500; // Hiển thị vụ nổ trong 500ms
 
     Enemy(int startX, int startY, SDL_Renderer* renderer);
     ~Enemy();
+
     void update();
     void reset();
+    void triggerExplosion(); // Khai báo hàm
+    void updateExplosion();  // Khai báo hàm
+    void renderExplosion(SDL_Renderer* renderer);
     SDL_Rect getRect();
+    void shoot(vector<Bullet>& bullets, SDL_Renderer* renderer); // Hàm bắn đạn
 };
 
 #endif
