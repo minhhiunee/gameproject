b8b2d2194d00338b25343be70fbdccc91bcb6447
Nguyễn Minh Hiếu <88912588+minhhiunee@users.noreply.github.com>
2025-04-22 16:29:15 +0700

    Update Object.cpp

--- Object.cpp
+++ Object.cpp
@@ -5,22 +5,19 @@
 using namespace std;
 
 Object::Object(int windowWidth, int windowHeight, SDL_Renderer* renderer)
-    : x(windowWidth), y(windowHeight - 330), active(true) {
-    // Tải 5 texture vật cản
+    : x(windowWidth), y(windowHeight - 330), speed(5), active(true) {
     textures.push_back(IMG_LoadTexture(renderer, "tree 1.png"));
     textures.push_back(IMG_LoadTexture(renderer, "tree 2.png"));
     textures.push_back(IMG_LoadTexture(renderer, "stone 1.png"));
     textures.push_back(IMG_LoadTexture(renderer, "stone 2.png"));
     textures.push_back(IMG_LoadTexture(renderer, "banana skin.png"));
 
-    // Kiểm tra lỗi khi tải texture
     for (size_t i = 0; i < textures.size(); ++i) {
         if (!textures[i]) {
             SDL_Log("Không thể tải texture object%zu.png: %s", i + 1, IMG_GetError());
         }
     }
 
-    // Chọn ngẫu nhiên texture ban đầu
     currentTexture = textures[rand() % 5];
 }
 
@@ -31,10 +28,14 @@ Object::~Object() {
 }
 
 void Object::update() {
-    x -= 5;
+    x -= speed;
 }
 
 void Object::reset(int windowWidth) {
-    x = windowWidth;
+    x = windowWidth + (rand() % 200 ); // Thêm khoảng cách ngẫu nhiên 
     currentTexture = textures[rand() % 5];
+}
+
+void Object::setSpeed(int newSpeed) {
+    speed = newSpeed;
 }
