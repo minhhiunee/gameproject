f2ce8d26bf8db0cbdf49bf084d9163c548a287d8
Nguyễn Minh Hiếu <88912588+minhhiunee@users.noreply.github.com>
2025-04-22 16:31:24 +0700

    Cập nhật lại trạng thái

--- main.h
+++ main.h
@@ -1,9 +1,12 @@
 #ifndef MAIN_H
 #define MAIN_H
-
+const int WINDOW_WIDTH = 1280;
+const int WINDOW_HEIGHT = 720;
 enum GameState {
-    RUNNING,
-    FLYING
+    START,      // Trạng thái màn hình Start
+    RUNNING,    // Trạng thái chạy
+    FLYING,     // Trạng thái bay
+    GAMEOVER    // Trạng thái Game Over
 };
 
 #endif
