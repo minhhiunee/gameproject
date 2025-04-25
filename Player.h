e2d3dc5253bc88b64b8e95f917bea16cf84568e4
Nguyễn Minh Hiếu <88912588+minhhiunee@users.noreply.github.com>
2025-04-25 23:24:47 +0700

    Update Player.h

--- Player.h
+++ Player.h
@@ -14,7 +14,7 @@ public:
     int height;
     int flyingWidth;
     int flyingHeight;
-    int speed; // Tốc độ di chuyển trong FLYING
+    int speed; // Tốc độ mặc định trong FLYING 
     int runningSpeed; // Tốc độ chạy trong RUNNING
     int score;
     int hp;
@@ -29,12 +29,7 @@ public:
     Player(int startX, int startY, SDL_Renderer* renderer);
     ~Player();
     void updateJump();
-    void movePlane(const Uint8* keystates) {
-        if (keystates[SDL_SCANCODE_UP]) this->y -= this->speed;
-        if (keystates[SDL_SCANCODE_DOWN]) this->y += this->speed;
-        if (this->y < 0) this->y = 0;
-        if (this->y > WINDOW_HEIGHT - flyingHeight) this->y = WINDOW_HEIGHT - flyingHeight;
-    }
+    void movePlane(const Uint8* keystates, float speed); // Thêm tham số speed
     void updateAnimation();
     SDL_Rect getRect(GameState state);
     SDL_Texture* getCurrentTexture(GameState state);
@@ -42,7 +37,7 @@ public:
     bool isAlive() const;
     void setRunningSpeed(int newSpeed);
     int getRunningSpeed() const { return runningSpeed; }
-    void reset(int startX, int startY); // Hàm mới để đặt lại trạng thái
+    void reset(int startX, int startY);
 };
 
 #endif
